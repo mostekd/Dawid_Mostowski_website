d0ae563ec2fe6683ca378e75c1f19ed3a32bd4cb
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-04 20:43:52 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Animacja przy najechaniu na ikony
 document.querySelectorAll('.link').forEach(link => {
     link.addEventListener('mouseover', () => {
         link.style.color = "#8be88a";
@@ -6,4 +5,14 @@ document.querySelectorAll('.link').forEach(link => {
     link.addEventListener('mouseout', () => {
         link.style.color = "#51ca52";
     });
+});
+window.addEventListener('scroll', function() {
+    const mainSection = document.querySelector('.main');
+    const offset = mainSection.getBoundingClientRect().top;
+    
+    if (offset < window.innerHeight / 2) {
+        mainSection.style.color = "#8be88a";
+    } else {
+        mainSection.style.color = "#d3d3d3";
+    }
 });
